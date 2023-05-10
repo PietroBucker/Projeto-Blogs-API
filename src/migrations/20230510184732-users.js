4001ad030dcb902067a16c9c4be19fcddb4ddbd3
Pietro Bucker <pietro_177@hotmail.com>
2023-05-10 16:26:34 -0300

    criado migration users e categories

---
+++ src/migrations/20230510184732-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  /**
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('Sequelize')} Sequelize   
+   * */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+
+      },
+      display_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      image: {
+
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
